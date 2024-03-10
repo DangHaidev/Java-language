84681e3bcc26e5645303b2523ddee3091f9b4a22
Hai Dang <dangd408@gmail.com>
2024-03-10 23:08:41 +0700

    descript

--- Horner_PolynomialEvaluation.java
+++ Horner_PolynomialEvaluation.java
@@ -4,13 +4,13 @@ public class Horner_PolynomialEvaluation {
         int result = poly[0];
         for(int i = 1; i < n ; i++)
         {
-            result = result*x + poly[i];
+            result = result*x + poly[i];  /// horner --> ((2x + 0x)x + 4)x - 1
         }
         return result;
     }
     public static void main(String[] args) {
-        int[] poly = {2,0,4,-1};
-        int x = 2;
+        int[] poly = {2,0,4,-1}; // 2x³ + 0x² + 4x -1  horner --> ((2x + 0x)x + 4)x - 1
+        int x = 2; //x(2)
         int n = poly.length;
         System.out.println(horner(poly, n, x));
     }
